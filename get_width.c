13cd0fd860c8c2c4e51ba61e79d8a697fcc89ab4
jotham333 <jothamjosiah10@gmail.com>
2023-03-31 13:28:14 -0700

    ALX

---
+++ get_width.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
+ */
